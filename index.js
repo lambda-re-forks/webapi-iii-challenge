0205f02ef3b6f5a16735f95c1fd0f1853ad87abb
clem9281 <clem9281@gmail.com>
2019-07-10 13:01:49 -0700

    added basic middleware, and server listening

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code away!
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("<h1>WebApi-III-Challenge: Web20</h1>"));
+app.use("/api/users", userRouter);
+app.use("/api/posts", postRouter);
+const port = process.env.PORT || 4000;
+app.listen(port, console.log(`Sever running on port ${port}`));
