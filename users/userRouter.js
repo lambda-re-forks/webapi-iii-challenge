6499146001d72c5055783800562d8e5ba13d9db2
clem9281 <clem9281@gmail.com>
2019-07-11 08:23:36 -0700

    added post for user and post NEEDS TESTING

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,11 +1,49 @@
 const express = require("express");
 const userDb = require("./userDb");
+const postDb = require("../posts/postDb");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {});
+router.post("/", validateUser, async (req, res) => {
+  try {
+    const userId = await userDb.insert(req.body);
+    const user = await userDb.findById(userId.id);
+    if (user) {
+      res.status(201).json(user);
+    } else {
+      res.status(404).json({
+        errorMessage: "There was a problem finding the user we just added"
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      errorMessage:
+        "Something went wrong when adding that entry to the database"
+    });
+  }
+});
 
-router.post("/:id/posts", (req, res) => {});
+router.post("/:id/posts", validatePost, async (req, res) => {
+  const id = req.params.id;
+  const newPost = { ...req.body, user_id: id };
+  try {
+    const postId = await postDb.insert(newPost);
+    const post = await postDb.findById(newPost.id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({
+        errorMessage: "There was a problem finding the user we just added"
+      });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        errorMessage: "Something went wrong adding that post to the database"
+      });
+  }
+});
 
 router.get("/", async (req, res) => {
   try {
@@ -83,6 +121,15 @@ function validateUser(req, res, next) {
   }
 }
 
-function validatePost(req, res, next) {}
+function validatePost(req, res, next) {
+  const post = req.body;
+  if (!post) {
+    res.status(400).json({ message: "missing post data" });
+  } else if (!post.text) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
