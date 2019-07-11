2f77fa46425bf733837ff4bf6400a0e6364beb5f
clem9281 <clem9281@gmail.com>
2019-07-10 19:53:23 -0700

    wrote get requests, started writing middleware NEEDS TESTING

--- index.js
+++ index.js
@@ -15,5 +15,12 @@ app.use(express.json());
 app.get("/", (req, res) => res.send("<h1>WebApi-III-Challenge: Web20</h1>"));
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
+
+
 const port = process.env.PORT || 4000;
 app.listen(port, console.log(`Sever running on port ${port}`));
+
+
+
+
+
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const userDb = require("./userDb");
 
 const router = express.Router();
 
@@ -6,11 +7,45 @@ router.post("/", (req, res) => {});
 
 router.post("/:id/posts", (req, res) => {});
 
-router.get("/", (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const users = await userDb.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await userDb.getById(id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res
+        .status(404)
+        .json({ errorMessage: "We could not find a user at that id" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      errorMessage: "Something went wrong, we could not get a user at that id"
+    });
+  }
+});
 
-router.get("/:id/posts", (req, res) => {});
+router.get("/:id/posts", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const posts = await userDb.getUserPosts(id);
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({
+      errorMessage:
+        "Something went wrong, we could not get the posts for that user"
+    });
+  }
+});
 
 router.delete("/:id", (req, res) => {});
 
@@ -18,9 +53,35 @@ router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {}
+async function validateUserId(req, res, next) {
+  const id = req.params.id;
+  try {
+    const user = await userDb.findById(id);
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(400).json({
+        errorMessage: "We're sorry, we could not find a user at that id"
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      errorMessage: "Something went wrong."
+    });
+  }
+}
 
-function validateUser(req, res, next) {}
+function validateUser(req, res, next) {
+  const user = req.body;
+  if (!user) {
+    res.status(400).json({ message: "missing user data" });
+  } else if (!user.name) {
+    res.status(400).json({ message: "missing required name field" });
+  } else {
+    next();
+  }
+}
 
 function validatePost(req, res, next) {}
 
